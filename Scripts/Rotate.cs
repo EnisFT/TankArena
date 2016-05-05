5ab42cd0fd3515442a6e9dc9fe80614ab2b2726c
EnisFT <jordane.perso@gmail.com>
2016-05-05 10:57:07 +0200

    Ajout script rotation avec la sourie

---
+++ Scripts/Rotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    public float rotateSpeed;
+
+    private Vector3 mousePos;
+    private float targetAngle;
+	
+	void Update () {
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo, 100)) {
+            Vector3 D = hitInfo.point - transform.position;
+            Quaternion rot = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(D), rotateSpeed * Time.deltaTime);
+            transform.rotation = rot;
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+
+    }
+}
