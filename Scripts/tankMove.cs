d5b8f41aca1015d351f95f32f1eb465438dfa646
xVyn <xavierfoot@live.fr>
2016-05-05 11:29:15 +0200

    Ajout des scripts de mouvement de la coque et du canon.

---
+++ Scripts/tankMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tankMove : MonoBehaviour {
+
+	public float moveSpeed;
+	public float turnSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKey(KeyCode.Z))
+		{
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.Q))
+		{
+			transform.Rotate(-Vector3.up, turnSpeed * Time.deltaTime);
+		}
+
+		transform.eulerAngles =  new Vector3(0, transform.eulerAngles.y, 0);
+	}
+}
