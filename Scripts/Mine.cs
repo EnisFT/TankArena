904abe1fde1f149fbb6f09f70a2e3a6a5d94f2ff
PastyBully <pasty.bully@gmail.com>
2016-05-05 17:33:49 +0200

    Add Mine.cs

---
+++ Scripts/Mine.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mine : MonoBehaviour {
+
+    public GameObject MinePrefab;
+    private float seconde = 0;
+    
+    // Use this for initialization
+    void Start () {
+      
+    }
+
+    // Update is called once per frame
+    void Update() {
+    seconde += Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space) && seconde >= 4)
+            { 
+                GameObject mineGo = Instantiate(MinePrefab);
+                mineGo.transform.position = transform.position;
+                seconde = 0;
+            }
+            
+          
+    }
+
+    void OnTriggerEnter(Collider colid) {
+        if (seconde >= 4)
+        {
+            Destroy(colid.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
